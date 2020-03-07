9c177c2b6dd8b58e39c38a0f29ce6d2893b02db7
Joey <chjalome>
2020-03-07 03:45:20 +0200

    update

--- redcap_jobscopy.php
+++ redcap_jobscopy.php
@@ -44,11 +44,11 @@
                     <br/>
                     <h5 class="h5_left">Job Type:</h5>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Full time</li>
-                        <li class="list-group-item">Contract</li>
-                        <li class="list-group-item">Temporary</li>
-                        <li class="list-group-item">Part-time</li>
-                        <li class="list-group-item">Permanent</li>
+                        <li class="list-group-item" onclick="filter('fulltime')">Full time</li>
+                        <li class="list-group-item" onclick="filter('contract')">Contract</li>
+                        <li class="list-group-item" onclick="filter('temp')">Temporary</li>
+                        <li class="list-group-item" onclick="filter('parttime')">Part-time</li>
+                        <li class="list-group-item" onclick="filter('perm')">Permanent</li>
                     </ul>
 					
 					 <h5 class="h5_left">Areas of Expertise:</h5>
@@ -102,59 +102,14 @@
             <div class="col-sm-7 row2">
             
             <?php
-                $data = array(
-                    'token' => '60A3D69D389B0F9BAD605C48AA200417',
-                    'content' => 'record',
-                    'format' => 'json',
-                    'type' => 'flat',
-                    'csvDelimiter' => '',
-                    'rawOrLabel' => 'raw',
-                    'rawOrLabelHeaders' => 'raw',
-                    'exportCheckboxLabel' => 'false',
-                    'exportSurveyFields' => 'false',
-                    'exportDataAccessGroups' => 'false',
-                    'returnFormat' => 'json'
-                );
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, 'https://redcap.core.wits.ac.za/redcap/api/');
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($ch, CURLOPT_VERBOSE, 0);
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-                curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-                curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-                curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
-                $output = curl_exec($ch);
-                // print $output;
-                $red_cap_job_list = json_decode($output, true);
-                for ($x = 0; $x <= count($red_cap_job_list)-1; $x++) {
-
-                    $post_str = '
-                    <br/>
-                    <div class="card">
-                            <h5 class="card-header">'.$red_cap_job_list[$x]["previous_current_position"].'</h5>
-                            <div class="card-body">
-                            <h5 class="card-title">'.$red_cap_job_list[$x]["initials_published_on_webs"].'</h5>
-                            <p class="card-text">'.$red_cap_job_list[$x]["brief_summary"].'</p>
-                            <a href="mailto:'.$red_cap_job_list[$x]["email"].'" class="btn btn-primary">contact</a>
-                            </div>
-                            <div class="card-footer text-muted">
-                            '.$red_cap_job_list[$x]["last_name"].',
-                            '.$red_cap_job_list[$x]["date"].'
-                            </div>
-                    </div>
-                    ';
-                    echo $post_str;
-                    // print json_encode($red_cap_job_list[$x]["email"]);
+                if(isset($_GET["fulltime"])){
+                  echo "fulltime";
                 }
 
-                curl_close($ch);
-            ?>
-
-
-
+                if(isset($_GET["parttime"])){
+                  echo "part time";
+                }
+              ?> 
 
             </div>
             <div class="col-sm row3">
@@ -175,5 +130,13 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<script>
+  function filter(filt){
+    console.log(filter);
+  }
+</script>
+
 </body>
 </html>
